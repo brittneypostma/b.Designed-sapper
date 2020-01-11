2fa5835022503e6575199764372be45be7d1ad33
Brittney <brittney.postma@hotmail.com>
2020-01-11 12:24:37 -0500

    fixed home page text and added contact envelope

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,14 +1,61 @@
+<script>
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    let flap = document.getElementById("flap");
+    let envelope = document.getElementById("envelope");
+
+    envelope.addEventListener("click", function() {
+      flap.classList.toggle("open");
+    });
+  });
+</script>
+
 <style>
-  div {
-    margin: 0 auto;
-    width: 75%;
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 75%;
+  }
+
+  .envelope {
+    position: relative;
+    width: 500px;
+    height: 300px;
+    background: linear-gradient(#333, #000) 0 50%;
+    top: 10%;
+  }
+  .flap {
+    transition: 0.5s all linear;
+    fill: rgb(255, 255, 255);
+    animation: open-flap 2s ease-in-out forwards;
+    transform-origin: 0 0 0;
+  }
+
+  @keyframes open-flap {
+    0% {
+      transform: rotateX(0deg);
+    }
+    100% {
+      transform: rotateX(180deg);
+    }
   }
 </style>
 
 <svelte:head>
   <title>Contact</title>
 </svelte:head>
-
-<div>
-  <h1>contact</h1>
+<div class="container">
+  <div id="envelope" class="envelope">
+    <svg
+      id="flap"
+      class="flap"
+      height="50%"
+      width="100%"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="none">
+      <polygon points="0,0 100,0 50,100" />
+    </svg>
+  </div>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,11 +2,11 @@
   import { onMount } from "svelte";
 
   const changingTextArray = [
-    "Mom",
     "Developer",
     "Designer",
     "Marketer",
-    "Freelancer"
+    "Freelancer",
+    "Mom"
   ];
 
   let curIdx = 0;
@@ -22,27 +22,46 @@
 </script>
 
 <style>
-  div {
+  .cont {
     margin: 0 auto;
     width: 75%;
     height: 80%;
     display: grid;
     justify-content: center;
-    justify-items: flex-start;
-    align-items: center;
     align-content: center;
+    font-size: 2.5vh;
   }
 
-  h1,
-  h2,
-  span {
+  .ind {
+    display: flex;
+  }
+
+  .bgText {
     text-align: center;
     font-style: italic;
-    letter-spacing: 5px;
+    letter-spacing: 7px;
     background: linear-gradient(#262626, #eaeaea, #262626);
     -webkit-background-clip: text;
     background-clip: text;
     color: transparent;
+  }
+
+  .hlText {
+    font-style: italic;
+    margin: 0 0.5em;
+    transition: all 0.5s linear;
+    background-color: rgba(0, 255, 255, 0.7);
+    padding: 0 0.5em;
+    text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5), 0px 0px 1px #111,
+      #333 0.006em 0.006em 0.007em, #9c9c9c 1px 1px 1px;
+  }
+
+  .name {
+    margin-left: 10px;
+    font-style: italic;
+    color: rgba(0, 255, 255, 0.7);
+    text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5), 0px 0px 1px #111,
+      #333 0.006em 0.006em 0.007em, #9c9c9c 1px 1px 1px;
   }
 
   a {
@@ -68,15 +87,28 @@
   <title>b.Designed</title>
 </svelte:head>
 
-<div>
-  <h1>Hello, I'm Brittney Postma,</h1>
-  <h2>owner at b.Designed.</h2>
-  <h1>
-    I'm a
-    <span>{changingTextArray[curIdx]}</span>
-  </h1>
+<div class="cont">
+  <div class="ind">
+    <h1 class="bgText">Hello, I'm</h1>
+    <h1>
+      <span class="name">Brittney Postma</span>
+    </h1>
+  </div>
 
-  <a href="portfolio">
-    <span>View My Work</span>
-  </a>
+  <div class="ind">
+    <h2 class="bgText">owner at b.Designed</h2>
+  </div>
+
+  <div class="ind">
+    <h1 class="bgText">I'm a</h1>
+    <h1>
+      <span class="hlText">{changingTextArray[curIdx]}</span>
+    </h1>
+  </div>
+
+  <div class="ind">
+    <a href="portfolio">
+      <span class="bgText">View My Work</span>
+    </a>
+  </div>
 </div>
