3165e1ec05b9691c998a2fda074c56b8634bf460
Brittney <brittney.postma@hotmail.com>
2020-01-14 21:11:56 -0500

    working on nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -7,7 +7,18 @@
     padding: 2px;
   }
 
-  .nav-cont {
+  /* #nav-check {
+    display: none;
+  }
+
+  .nav-menu {
+    display: none;
+  } */
+  .nav-links {
+    position: fixed;
+    z-index: 10;
+    margin-bottom: 1em;
+    width: 100%;
     display: grid;
     grid-template-columns: 150px 150px 150px 150px 150px;
     justify-content: center;
@@ -21,7 +32,7 @@
   a::before {
     content: "";
     position: absolute;
-    z-index: 100;
+    z-index: 11;
     transition: transform 0.3s ease;
     left: 12%;
     bottom: 0;
@@ -77,7 +88,7 @@
     content: "";
     position: absolute;
     top: 0px;
-    z-index: 101;
+    z-index: 11;
     left: 2px;
     width: calc(100% - 4px);
     height: 56px;
@@ -103,10 +114,172 @@
   a:last-child::after {
     border-radius: 0 0.7em 0.7em 0;
   }
+
+  /* @media (max-width: 800px) {
+    .nav-menu {
+      display: grid;
+      grid-template-columns: 1fr;
+      justify-content: flex-end;
+    }
+
+    label {
+      display: inline-block;
+      width: 50px;
+      height: 50px;
+      padding: 13px;
+    }
+
+    label:hover,
+    #nav-check:checked ~ label {
+      background-color: rgba(0, 0, 0, 0.3);
+    }
+
+    .nav-links {
+      position: absolute;
+      display: block;
+      width: 100%;
+      background-color: #333;
+      height: 0px;
+      transition: all 0.3s ease-in;
+      overflow-y: hidden;
+      top: 50px;
+      left: 0px;
+    }
+
+    .nav-links > a {
+      display: block;
+      width: 100%;
+    }
+
+    #nav-check:not(:checked) ~ .nav-links {
+      height: 0px;
+    }
+
+    #nav-check:checked ~ .nav-links {
+      height: calc(100vh - 50px);
+      overflow-y: auto;
+    }
+  } */
 </style>
 
+<!-- <script>
+  export let segment;
+</script><style>
+  .nav {
+    width: 100%;
+    background-color: #4d4d4d;
+    position: fixed;
+    z-index: 10;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .nav-title {
+    display: flex;
+    padding: 0.5em;
+  }
+
+  .nav-btn {
+    display: none;
+  }
+
+  .nav-links {
+    display: flex;
+    font-size: 18px;
+  }
+
+  .nav-links > a {
+    padding: 0 2vw;
+    text-decoration: none;
+    color: #efefef;
+  }
+
+  .nav-links > a:hover {
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+
+  #nav-check {
+    display: none;
+  }
+
+  @media (max-width: 800px) {
+    .nav-menu {
+      width: 50px;
+      height: 25px;
+    }
+    .nav-btn {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .nav-btn > label {
+      display: flex;
+      width: 50px;
+      height: 50px;
+      padding: 13px;
+      justify-content: center;
+      align-items: center;
+    }
+    .nav-btn > label:hover,
+    .nav #nav-check:checked ~ .nav-btn > label {
+      background-color: rgba(0, 0, 0, 0.3);
+    }
+    .nav-links {
+      position: absolute;
+      display: grid;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      background-color: #333;
+      height: 0px;
+      transition: all 0.3s ease-in;
+      overflow-y: hidden;
+      text-align: center;
+      top: 76px;
+      left: 0px;
+    }
+    .nav-links > a {
+      display: block;
+      width: 100%;
+    }
+    #nav-check:not(:checked) ~ .nav-links {
+      height: 0px;
+    }
+    #nav-check:checked ~ .nav-links {
+      height: 300px;
+      overflow-y: auto;
+    }
+  }
+</style><nav>
+  <div class="nav">
+    <input type="checkbox" id="nav-check" />
+    <div class="nav-title">
+      <img src="favicon.ico" alt="logo" class="logo" />
+    </div>
+
+    <div class="nav-btn">
+      <label for="nav-check">
+        <img src="nav-menu.png" alt="nav menu button" class="nav-menu" />
+      </label>
+    </div>
+
+    <div class="nav-links">
+      <a class:selected={segment === undefined} href=".">home</a>
+
+      <a class:selected={segment === 'about'} href="about">about</a>
+
+      <a class:selected={segment === 'portfolio'} href="portfolio">portfolio</a>
+
+      <a rel="prefetch" class:selected={segment === 'blog'} href="blog">blog</a>
+
+      <a class:selected={segment === 'contact'} href="contact">contact</a>
+    </div>
+  </div>
+</nav> -->
 <nav>
-  <div class="nav-cont">
+  <!-- <input type="checkbox" id="nav-check" /> -->
+  <div class="nav-links">
     <a class:selected={segment === undefined} href=".">home</a>
 
     <a class:selected={segment === 'about'} href="about">about</a>
@@ -117,5 +290,10 @@
 
     <a class:selected={segment === 'contact'} href="contact">contact</a>
   </div>
-
+  <!-- <div class="nav-menu">
+    <label id="nav-check" for="nav-check">
+      <img src="nav-menu.png" alt="nav menu button" class="nav-btn" />
+      <p>menu</p>
+    </label>
+  </div> -->
 </nav>
