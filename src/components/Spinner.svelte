22b6f574619ea049ad95dcbe1be50cc48db5b68f
Brittney <brittney.postma@hotmail.com>
2020-01-15 21:28:10 -0500

    contact form

--- src/components/Spinner.svelte
+++ src/components/Spinner.svelte
@@ -1,5 +1,5 @@
 <style>
-  .spinner-box {
+  .spinner {
     width: 100%;
     height: 100%;
     margin: 0 auto;
@@ -9,71 +9,115 @@
     background-color: transparent;
   }
 
-  .leo-border-1 {
-    position: absolute;
-    width: 150px;
-    height: 150px;
+  .ring {
+    width: 375px;
+    height: 375px;
+    margin-top: 7%;
     padding: 3px;
     display: flex;
     justify-content: center;
     align-items: center;
     border-radius: 50%;
     background: rgb(63, 249, 220);
-    background: linear-gradient(
-      0deg,
-      rgba(63, 249, 220, 0.1) 33%,
-      rgba(63, 249, 220, 1) 100%
-    );
-    animation: spin3D 1.8s linear 0s infinite;
-  }
-
-  .leo-core-1 {
-    width: 100%;
-    height: 100%;
-    background-color: #37474faa;
-    border-radius: 50%;
-  }
-
-  .leo-border-2 {
-    position: absolute;
-    width: 150px;
-    height: 150px;
-    padding: 3px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 50%;
-    background: rgb(63, 249, 220);
-    background: linear-gradient(
-      0deg,
-      rgba(255, 171, 145, 0.1) 33%,
-      rgba(255, 171, 145, 1) 100%
+    background: radial-gradient(
+      transparent 2%,
+      transparent,
+      rgba(0, 255, 255, 0.9)
     );
     animation: spin3D 2.2s linear 0s infinite;
   }
 
-  .leo-core-2 {
+  .ring-center {
     width: 100%;
     height: 100%;
-    background-color: #ffab91aa;
+    background-color: transparent;
     border-radius: 50%;
   }
 
   @keyframes spin3D {
     from {
-      transform: rotate3d(0.5, 0.5, 0.5, 360deg);
+      transform: rotate3d(1, 1, 1, 360deg);
     }
     to {
       transform: rotate3d(0deg);
     }
   }
+
+  .planet {
+    width: 300px;
+    height: 300px;
+    position: absolute;
+    border-radius: 50%;
+    overflow: hidden;
+    box-shadow: 0 0 60px -20px rgba(106, 185, 190, 0.72),
+      -14px -15px 40px -10px rgba(97, 97, 97, 0.23);
+    left: 50%;
+    top: 50%;
+    margin: -150px;
+  }
+  .planet .mask {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    box-shadow: inset -10px -10px 40px #032325,
+      inset 10px 10px 30px -10px rgba(159, 255, 226, 0.6);
+    border-radius: 50%;
+  }
+  .planet .clouds {
+    background: url(../clouds.png) repeat-x;
+    width: 300px;
+    height: 300px;
+    position: absolute;
+    border-radius: 50%;
+    animation: translateBackground 30s infinite linear;
+    opacity: 0.4;
+  }
+  .planet .background {
+    animation: translateBackground 40s infinite linear;
+    background-image: url(../planet.png),
+      radial-gradient(rgb(2, 231, 231), rgb(0, 66, 66));
+    background-repeat: repeat-x;
+    background-color: rgb(0, 255, 255);
+    width: 300px;
+    height: 300px;
+    position: absolute;
+    border-radius: 50%;
+  }
+  .planet .wrap {
+    width: 300px;
+    height: 300px;
+    position: absolute;
+    border-radius: 50%;
+    animation: rotatePlanet 150s infinite linear;
+  }
+  @keyframes translateBackground {
+    0% {
+      background-position-x: 0;
+    }
+    100% {
+      background-position-x: -1000px;
+    }
+  }
+  @keyframes rotatePlanet {
+    0% {
+      transform: rotateZ(0);
+    }
+    100% {
+      transform: rotateZ(-360deg);
+    }
+  }
+
+  /* Spinner inspiration from https://codepen.io/artyom-ivanov/pen/QjOYbO*/
 </style>
 
-<div class="spinner-box">
-  <div class="leo-border-1">
-    <div class="leo-core-1" />
-  </div>
-  <div class="leo-border-2">
-    <div class="leo-core-2" />
+<div class="spinner">
+  <div class="ring" />
+  <div class="ring-center" />
+  <div class="planet">
+    <div class="wrap">
+      <div class="background" />
+      <div class="clouds" />
+    </div>
+    <div class="mask" />
   </div>
 </div>
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -21,6 +21,57 @@
     transform-origin: 0 0 0;
   }
 
+  .form {
+    position: absolute;
+    top: 0;
+    max-width: 420px;
+  }
+
+  input[type="text"],
+  input[type="email"] {
+    color: white;
+    font-family: Helvetica, Arial, sans-serif;
+    font-weight: 500;
+    font-size: 18px;
+    border-radius: 5px;
+    line-height: 22px;
+    background-color: transparent;
+    border: 2px solid #cc6666;
+    transition: all 0.3s;
+    padding: 13px;
+    margin-bottom: 15px;
+    width: 100%;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  input[type="textarea"] {
+    height: 150px;
+    width: 100%;
+    line-height: 150%;
+    resize: vertical;
+  }
+
+  input[type="submit"] {
+    font-family: "Montserrat", Arial, Helvetica, sans-serif;
+    width: 100%;
+    background: #cc6666;
+    border-radius: 5px;
+    border: 0;
+    cursor: pointer;
+    color: white;
+    font-size: 24px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    transition: all 0.3s;
+    margin-top: -4px;
+    font-weight: 700;
+  }
+
+  input[type="submit"]:hover {
+    background: #cc4949;
+  }
+
   @keyframes open-flap {
     0% {
       transform: rotateX(0deg);
@@ -68,5 +119,14 @@
       preserveAspectRatio="none">
       <polygon points="0,0 100,0 50,100" />
     </svg>
+    <form class="form" name="contact" method="POST" data-netlify="true">
+      <input type="text" name="first-name" placeholder="First Name" />
+      <input type="text" name="last-name" placeholder="Last Name" />
+      <input type="email" name="email" placeholder="Email" />
+      <input type="text" name="subject" placeholder="Subject" />
+      <input type="textarea" name="message" placeholder="Message" />
+      <input type="submit" value="Send" />
+    </form>
   </div>
+
 </div>
