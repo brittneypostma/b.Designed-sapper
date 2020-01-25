d810eae75d4e482484db56709923d39bfbf563ce
Brittney <brittney.postma@hotmail.com>
2020-01-25 15:24:14 -0500

    revert

--- rollup.config.js
+++ rollup.config.js
@@ -15,7 +15,7 @@ const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
-// const dedupe = importee => importee === 'svelte' || importee.startsWith('svelte/');
+const dedupe = importee => importee === 'svelte' || importee.startsWith('svelte/');
 
 const markdown = () => ({
 	transform(md, id) {
@@ -84,11 +84,9 @@ export default {
 				generate: 'ssr',
 				dev
 			}),
-			resolve(
-				// 	{
-				// 	dedupe
-				// 
-			),
+			resolve({
+				dedupe
+			}),
 			commonjs(),
 			markdown(),
 		],
