e3eb7e085ee84dd5b11aa8656cc28ccf11659da0
sballgirl11 <brittney.postma@hotmail.com>
2020-01-25 09:05:15 -0500

    testing blog

--- src/routes/blog/index.json.js
+++ src/routes/blog/index.json.js
@@ -1,19 +1,21 @@
-import posts from './_posts.js';
+import posts from "./_posts.js";
 
-const contents = JSON.stringify(posts.map(post => {
-	return {
-		title: post.title,
-		slug: post.slug,
-		printDate: post.printDate,
-		image: post.image,
-		alt: post.alt,
-	};
-}));
+const contents = JSON.stringify(
+  posts.map(post => {
+    return {
+      title: post.title,
+      slug: post.slug
+      // printDate: post.printDate,
+      // image: post.image,
+      // alt: post.alt,
+    };
+  })
+);
 
 export function get(req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'application/json'
-	});
+  res.writeHead(200, {
+    "Content-Type": "application/json"
+  });
 
-	res.end(contents);
+  res.end(contents);
 }
